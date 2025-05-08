5e700faea969f1ad6d7f4433a066e6a57de05dc5
Codyvb <37961637+codyvb@users.noreply.github.com>
2025-05-08 05:00:40 -0600

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout(props: { children: ReactNode }) {
   )
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
       <body className={inter.className}>
         <Providers initialState={initialState}>{props.children}</Providers>
       </body>
